004b33e0f7f6a63fc692509826ef0bcd44df3465
Dissy <dissyvargas2@gmail.com>
2023-11-17 12:25:38 -0600

    added custom view to admin/needs file upload

--- project3/checklist_admin/admin.py
+++ project3/checklist_admin/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-
+from django.urls import include, path
+from django.template.response import TemplateResponse
 
 # Register your models here.
 class ChecklistAdmin(admin.AdminSite):
     site_header = "Checklist Administration"
-
+    def profile_view(self, request): 
+        request.current_app = self.name
+        context = self.each_context(request)
+        return TemplateResponse(request, "admin_profile.html", context)
+    def get_urls(self):
+        urls = super().get_urls()
+        url_patterns = [
+            path("admin_profile", self.admin_view(self.profile_view))
+        ]
+        return url_patterns + urls
+    
 
--- project3/project3/urls.py
+++ project3/project3/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 import degree_checklist.views
 from django.contrib import admin, auth
 #from checklist_admin.admin import admin_site
 from django.urls import include, path
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,6 @@ urlpatterns = [
     path('', include('degree_checklist.urls')),
     
 ]
+if settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
